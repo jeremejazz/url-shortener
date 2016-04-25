fc45cfac5fb396a77dcf8fbba051c20d9ee51796
career hub <Careerhubmoa@microsourcing.local>
2016-04-25 17:09:24 +0800

    404 error and code clean

--- app/controllers/url.php
+++ app/controllers/url.php
@@ -16,14 +16,14 @@ class url extends Controller{
 
  				$this->f3->set('link' , $hash);
  				$this->f3->set('view', 'shortened.htm');
- 				
+
 
  			}else{
  				$this->f3->reroute('/');
  			}
 				 
 		}
-		
+
 		 		//link redirect
 		public function r(){ 
 			
@@ -32,7 +32,17 @@ class url extends Controller{
 			$url = new \Url_model($this->db);
 
 			$result =  $url->getByHash($hash);
-			$this->f3->reroute($result[0]['url']);
+
+			if(empty($result[0]['url'])){
+
+				$this->f3->set("view",'error_404.htm');
+
+			}else{
+
+				$this->f3->reroute($result[0]['url']);
+				
+			}
+			
 		}	
 	 
 
